ef1cdbe1c824c5bc770db5047c0a3e453fda70e7
Stefan Gündhör <stefan@guendhoer.com>
2017-04-03 01:40:14 +0200

    add cache-control for static files (cache forever as we are using versioned urls)

--- src/main/java/org/ownchan/server/ui/core/config/WebMvcConfig.java
+++ src/main/java/org/ownchan/server/ui/core/config/WebMvcConfig.java
@@ -18,28 +18,49 @@
  *******************************************************************************/
 package org.ownchan.server.ui.core.config;
 
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang3.StringUtils;
 import org.ownchan.server.ui.core.ScanBaseMarker;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration("ownchan-server-ui-core-config")
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     String implementationVersion = ScanBaseMarker.class.getPackage().getImplementationVersion();
-    /*
-     * Locally, all versions are tolerated in the URL (because if the application is not started as a .jar, implementationVersion will be null),
-     * while in production environments the version of the dependency will be enforced.
-     */
-    String webBasePath = "/oc-ui-core/" + (implementationVersion == null ? "{version:[a-zA-Z0-9\\.\\-]+}" : implementationVersion);
+
+    String pathVersion;
+    CacheControl cacheControl;
+    if (StringUtils.isNotBlank(implementationVersion)) {
+      /*
+       * As we use versioning for the GUI URL paths, we can cache "forever".
+       * Browsers will re-validate when the resource's URL changes with every new version of the application.
+       */
+      cacheControl = CacheControl.maxAge(365, TimeUnit.DAYS);
+      pathVersion = implementationVersion;
+    } else {
+      /*
+       * If the application is started directly in the DEV IDE,
+       * we don't cache and allow all versions in the path.
+       */
+      cacheControl = CacheControl.noStore();
+      pathVersion = "{version:[a-zA-Z0-9\\.\\-]+}";
+    }
+
+    String webBasePath = "/ocn-ui-core/" + pathVersion;
     String localBasePath = "classpath:/ownchan-server-ui-core";
 
     registry.addResourceHandler(webBasePath + "/widgets/**")
-        .addResourceLocations(localBasePath + "/jqx/jqwidgets/");
+        .addResourceLocations(localBasePath + "/jqx/jqwidgets/")
+        .setCacheControl(cacheControl);
     registry.addResourceHandler(webBasePath + "/common/**")
-        .addResourceLocations(localBasePath + "/common/");
+        .addResourceLocations(localBasePath + "/common/")
+        .setCacheControl(cacheControl);
   }
 
 }
